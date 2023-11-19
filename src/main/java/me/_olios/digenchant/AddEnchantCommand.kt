51ce3ac0de9154438f21c634748d56b63f2e6298
Ariel <127093985+ariel3007@users.noreply.github.com>
2023-11-19 14:18:42 +0200

    Added option to change enchant level at AddEnchantCommand.kt set the data on the item on DigEnchantData.kt

--- src/main/java/me/_olios/digenchant/AddEnchantCommand.kt
+++ src/main/java/me/_olios/digenchant/AddEnchantCommand.kt
@@ -7,12 +7,15 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 class AddEnchantCommand(private val plugin: DigEnchant): CommandExecutor {
-    override fun onCommand(sender: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
+    override fun onCommand(sender: CommandSender, p1: Command, p2: String, args: Array<out String>?): Boolean {
         if (sender !is Player) return false
+
         val itemInHand = sender.inventory.itemInMainHand.type
         if (!itemInHand.isItem) return false
+        if (args.isNullOrEmpty() || args[0].toIntOrNull() == null) return false
+        val enchantLevel = args[0].toInt()
         val itemStack = sender.inventory.itemInMainHand
-        DigEnchantData(plugin, sender).heldItem(itemStack)
+        DigEnchantData(plugin, sender).heldItem(itemStack, enchantLevel)
         return true
     }
 }
--- src/main/java/me/_olios/digenchant/digEnchant/DigEnchantData.kt
+++ src/main/java/me/_olios/digenchant/digEnchant/DigEnchantData.kt
@@ -1,11 +1,16 @@
 package me._olios.digenchant.digEnchant
 
 import me._olios.digenchant.DigEnchant
+import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
 class DigEnchantData(private val plugin: DigEnchant, private val player: Player) {
-    fun heldItem(item: ItemStack) {
+    fun heldItem(item: ItemStack, enchantLevel: Int) {
         val itemMeta = item.itemMeta
+        val key = NamespacedKey(plugin, "enchantment")
+        itemMeta.persistentDataContainer.set(key, PersistentDataType.INTEGER, enchantLevel)
+        item.itemMeta = itemMeta
     }
 }
