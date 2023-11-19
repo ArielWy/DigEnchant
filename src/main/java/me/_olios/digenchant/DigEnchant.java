a5c6e1f45b96629e099888af710c819ab13af295
Ariel <127093985+ariel3007@users.noreply.github.com>
2023-11-19 13:06:20 +0200

    initial commit - the first commit

---
+++ src/main/java/me/_olios/digenchant/DigEnchant.java
@@ -0,0 +1,17 @@
+package me._olios.digenchant;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DigEnchant extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
